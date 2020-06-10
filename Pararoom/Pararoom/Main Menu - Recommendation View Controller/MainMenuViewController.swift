4b1fda383c7a8fcf70d2ead1334b8bf6fb02e6a3
Rommy Julius Dwidharma <62103999+rommyjulius@users.noreply.github.com>
2020-06-10 17:23:30 +0700

    Main Menu - To Gameplay
    Gif Issue (Debugger: Memory Issue)
    BGM in Main Menu not yet implemented
    Animation not yet implemented

---
+++ Pararoom/Pararoom/Main Menu - Recommendation View Controller/MainMenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainMenuViewController.swift
+//  Pararoom
+//
+//  Created by Rommy Julius Dwidharma on 10/06/20.
+//  Copyright © 2020 Maria Jeffina. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var menuView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        overrideUserInterfaceStyle = .dark
+        // Do any additional setup after loading the view.
+        backgroundImage.loadGif(name: "gameBg")
+        self.view.sendSubviewToBack(backgroundImage)
+        self.view.bringSubviewToFront(menuView)
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        performSegue(withIdentifier: "fyiOnboarding", sender: self)
+    }
+    
+    func loadImage() {
+    
+        
+    }
+    
+}
