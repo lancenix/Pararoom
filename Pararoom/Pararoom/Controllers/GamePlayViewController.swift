3d4f7be6f370003781939e5dfaf95859563cc085
Maria Jeffina <mariajeffina@gmail.com>
2020-06-12 13:07:33 +0700

    add testView

--- Pararoom/Pararoom/Controllers/GamePlayViewController.swift
+++ Pararoom/Pararoom/Controllers/GamePlayViewController.swift
@@ -39,6 +39,9 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var hammerButton: UIButton!
     @IBOutlet weak var deadFireballButton: UIButton!
     
+    @IBOutlet weak var TestView: UIView!
+    
+    
     var prologue = ["Well… Well… Well… \n Look Who’s Here!!!", "I can see you’re trapped, I know a way how to escape but there is one condition...", "That is if you help me find a soul fragment to revive my friend... I'll help you escape!!", "You can find the soul fragment by interacting from this room! \n Good Luck..."]
     var inventoryItem : [String] = ["", "", ""]
     let pinChoices = ["Heart", "Soul", "Fire"]
@@ -106,6 +109,10 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     }
     
     @IBAction func hideNodeInteractionView(_ sender: Any) {
+        if fireballIsAlive {
+            TestView.isHidden = false
+        }
+        
         NodeInteractionView.isHidden = true
         
         showPainting = false
@@ -114,9 +121,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         enterPINButton.isHidden = true
         showPainting = false
         
-        if fireballIsAlive {
-            endingSetup()
-        }
         
     }
     
@@ -313,6 +317,8 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         
         hammerButton.isHidden = true
         soulFragment.isHidden = true
+        
+        TestView.isHidden = true
     }
     
     //MARK: NPC game prologue dialog
