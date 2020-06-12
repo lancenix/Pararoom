fde8647e452659eb248bc76f19fb1731a8eae986
Maria Jeffina <mariajeffina@gmail.com>
2020-06-12 11:45:41 +0700

    finish interactions
    - reviving dead fireball
    - show portal when fireball is revived.

--- Pararoom/Pararoom/Controllers/GamePlayViewController.swift
+++ Pararoom/Pararoom/Controllers/GamePlayViewController.swift
@@ -36,6 +36,7 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var woodImage: UIButton!
     @IBOutlet weak var soulFragment: UIButton!
     @IBOutlet weak var hammerButton: UIButton!
+    @IBOutlet weak var deadFireballButton: UIButton!
     
     var convArray = ["Well… Well… Well… \n Look Who’s Here!!!", "I can see you’re trapped, I know a way how to escape but there is one condition...", "That is if you help me find a soul fragment to revive my friend... I'll help you escape!!", "You can find the soul fragment by interacting from this room! \n Good Luck..."]
     var inventoryItem : [String] = ["", "", ""]
@@ -43,18 +44,23 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     private let correctPIN = "34373"
     
     //MARK: Flags
-    var takeHammerFlag = false
+    var fragmentIsSelected = false
     var hammerIsSelected = false
+    var takeHammerFlag = false
     var takeFragmentFlag = false
+    private var woodDestroyedFlag = false
     private var flagPin = false
     private var showPainting = false
     var isSelected = true
     var gerakFrameKiri = true
+    private var fireballIsAlive = false
     
     
     let nodeBrankas = SCNNode()
     let nodeWoodboard = SCNNode()
     let nodeGrim = SCNNode()
+    let FireKiri = SCNNode()
+    let nodePortal = SCNNode()
     
     var animationProperty = UIViewPropertyAnimator()
     
@@ -73,18 +79,18 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         setupWoodboard()
         setupGrim()
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
         viewSetup()
         
         
         registerGestureRecognizers()
     }
+    
         
     @IBAction func woodPressed(_ sender: Any) {
         if hammerIsSelected{
             nodeWoodboard.isHidden = true
             woodImage.isHidden = true
+            woodDestroyedFlag = true
             soulFragment.isHidden = false
         }
         else {
@@ -101,6 +107,19 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         nodeInteractionMessage.isHidden = true
         enterPINButton.isHidden = true
         showPainting = false
+        
+    }
+    
+    @IBAction func skullRevived(_ sender: Any) {
+        if fragmentIsSelected {
+            FireKiri.geometry?.materials.first?.diffuse.contents = UIImage.gif(name: "FireballB_SFX")
+            fireballIsAlive = true
+            deadFireballButton.isHidden = true
+            ZoomedNodeImage.isHidden = false
+            ZoomedNodeImage.image = UIImage.gif(name: "FireballB_SFX")
+            nodeInteractionMessage.text = "I appreciate your effort."
+            nodePortal.isHidden = false
+        }
     }
     
     @IBAction func takeSoulFragment(_ sender: Any) {
@@ -137,7 +156,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         }
                     
         addAction.isEnabled = false
-                
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         
         alertController.addTextField(configurationHandler: { textField in
@@ -145,7 +163,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
             textField.keyboardType = .numberPad
             textField.addTarget(self, action: #selector(self.handleTextChanged), for: .editingChanged)
         })
-        
         alertController.addAction(addAction)
         alertController.addAction(cancelAction)
             
@@ -179,11 +196,11 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
             if !showPainting {
                 woodImage.isHidden = true
             }
+            ZoomedNodeImage.isHidden = false
             if hitResults.first?.node.name == "nodeBrangkas" {
                 
                 ZoomedNodeImage.image = (hitResults.first?.node.geometry?.materials.first?.diffuse.contents as! UIImage)
                 NodeInteractionView.isHidden = false
-//                PINChoicesCollectionView.isHidden = false
                 nodeInteractionMessage.isHidden = false
                 nodeInteractionMessage.text = "This safety box needs a PIN for it to be opened. What is it?"
                 
@@ -198,9 +215,10 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
             else if hitResults.first?.node.name == "nodePainting"{
                 showPainting = true
                 ZoomedNodeImage.image = UIImage(named: "painting_wood")
-                
-                
-                woodImage.isHidden = false
+                deadFireballButton.isHidden = true
+                if !woodDestroyedFlag {
+                    woodImage.isHidden = false
+                }
                 NodeInteractionView.isHidden = false
             }
             else if hitResults.first?.node.name == "grimRiper"{
@@ -212,20 +230,14 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
                 nodeInteractionMessage.isHidden = false
                 
                 nodeInteractionMessage.text = "What are the thing that you need?"
-                
-                
-                
             }
                 
-                else if hitResults.first?.node.name == "frameKiri"{
+            else if hitResults.first?.node.name == "frameKiri"{
                 
                 ZoomedNodeImage.image = UIImage(named: "frame question")
                 
                 let interval = 0.01
                 let manager = CMMotionManager()
-//                let frameKiriWidth = CGFloat(400)
-//                let frameKiriHeight = CGFloat(600)
-               
                
                 manager.deviceMotionUpdateInterval = interval
                 let queue = OperationQueue()
@@ -236,47 +248,33 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
                 let gravity = data.gravity
                     let rotation = atan2(gravity.x, gravity.y) - .pi
                     
-                    
-                    
-//                                    if data.attitude.roll >= 1.39 && data.attitude.roll <= 1.61 || data.attitude.roll >= -1.61 && data.attitude.roll <= -1.39{
-////                                        self.showPass()
-//
-//                                    }
-//
-
                     OperationQueue.main.addOperation {
                         self.ZoomedNodeImage?.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
                     }
                 })
-                
-                
-
-                
-                
-//                func SetImageView() {
-//                    if !gerakFrameKiri { return }
-//
-//                let iv = ZoomedNodeImage
-//
-//
-//                // center the image
-//                let x = (self.view.frame.width/2)-(frameKiriWidth/2)
-//                let y = (self.view.frame.height/2)-(frameKiriHeight/2)
-//                    iv?.frame = CGRect(x: x, y: y, width: frameKiriWidth, height: frameKiriHeight)
-//
-//                self.view.addSubview(iv!)
-//                self.ZoomedNodeImage = iv
-//
-//
-//                           }
-//                 SetImageView()
-                
-                              
                 NodeInteractionView.isHidden = false
             }
-            else{
-                ZoomedNodeImage.image = (hitResults.first?.node.geometry?.materials.first?.diffuse.contents as! UIImage)
+            else if hitResults.first?.node.name == "fireKanan" {
+                ZoomedNodeImage.image = UIImage.gif(name: "FireballA_SFX")
                 NodeInteractionView.isHidden = false
+                
+                nodeInteractionMessage.text = "So have you found it?"
+                nodeInteractionMessage.isHidden = false
+            }
+            else if hitResults.first?.node.name == "fireKiri" {
+                NodeInteractionView.isHidden = false
+                if fireballIsAlive {
+                    ZoomedNodeImage.image = UIImage.gif(name: "FireballB_SFX")
+                    nodeInteractionMessage.text = "I appreciate your effort."
+                }
+                else {
+                    ZoomedNodeImage.isHidden = true
+                    deadFireballButton.isHidden = false
+                    deadFireballButton.imageEdgeInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+                        nodeInteractionMessage.text = "This fireball needs a soul fragment to be revived"
+                        nodeInteractionMessage.isHidden = false
+                    
+                }
             }
         }
     }
@@ -301,12 +299,13 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         nodeInteractionMessage.isHidden = true
         enterPINButton.isHidden = true
         labelFrameKiri.isHidden = true
+        deadFireballButton.isHidden = true
         
         hammerButton.isHidden = true
         soulFragment.isHidden = true
         prevButtonHidden()
         
-         
+        nodePortal.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -329,20 +328,17 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     
     //MARK: Node setups
     func setupPortal(){
-
-          let planeGeometry = SCNPlane(width: 5, height: 10)
-                let material = SCNMaterial()
-                material.diffuse.contents = UIImageView.init(image: #imageLiteral(resourceName: "portal"))
-                planeGeometry.materials = [material]
-        //        let youDontWant = SCNText(string: "You Dont Want to Escape",extrusionDepth: 0)
-        //        youDontWant.materials = [SCNMaterial()]
-                
-                let nodePortal = SCNNode(geometry: planeGeometry)
-                nodePortal.position = SCNVector3(x : 0.1, y: 0.1, z : -5)
-//                nodeYouDont.scale = SCNVector3(x: -0.01, y: 0.01, z: -0.01)
-                sceneView.scene.rootNode.addChildNode(nodePortal)
-                
-                sceneView.autoenablesDefaultLighting = true
+        let planeGeometry = SCNPlane(width: 5, height: 10)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImageView.init(image: #imageLiteral(resourceName: "portal"))
+        planeGeometry.materials = [material]
+        
+        
+        nodePortal.geometry = planeGeometry
+        nodePortal.position = SCNVector3(x : 0.1, y: 0.1, z : -5)
+        sceneView.scene.rootNode.addChildNode(nodePortal)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     func setupYouDontText(){
@@ -350,8 +346,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         let material = SCNMaterial()
         material.diffuse.contents = UIImageView.init(image: #imageLiteral(resourceName: "YOU DONT"))
         planeGeometry.materials = [material]
-//        let youDontWant = SCNText(string: "You Dont Want to Escape",extrusionDepth: 0)
-//        youDontWant.materials = [SCNMaterial()]
         
         let nodeYouDont = SCNNode(geometry: planeGeometry)
         nodeYouDont.position = SCNVector3(x : 0.1, y: 0.1, z : 8)
@@ -367,7 +361,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         material.diffuse.contents = UIImage(named: "soul_fragment")
         planeGeometry.materials = [material]
         
-        
     }
     
     func setupBingkaiKiri(){
@@ -376,10 +369,6 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         let material = SCNMaterial()
         material.diffuse.contents = UIImageView.init(image: #imageLiteral(resourceName: "frame question"))
         planeGeometry.materials = [material]
-//        let planeGeometry = SCNPlane(width: 15, height: 15)
-//               let material = SCNMaterial()
-//               material.diffuse.contents = UIImage(named: "frame question")
-//               planeGeometry.materials = [material]
 
                let bingkaiKiri = SCNNode(geometry: planeGeometry)
         bingkaiKiri.name = "frameKiri"
@@ -428,29 +417,26 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
         material.diffuse.contents = UIImageView.init(image: #imageLiteral(resourceName: "grimreaper"))
         planeGeometry.materials = [material]
         
-//        let grim = SCNText(string: "grim",extrusionDepth: 0)
-//        grim.materials = [SCNMaterial()]
-//
-//        let nodeGrim = SCNNode()
         nodeGrim.geometry = planeGeometry
         nodeGrim.name = "grimRiper"
         nodeGrim.position = SCNVector3(x : -3, y: -1.5, z : -1)
         nodeGrim.rotation = SCNVector4Make(0, 1, 0, .pi / -2)
         nodeGrim.scale = SCNVector3(x: -0.01, y: 0.05, z: -0.3)
-//        nodeGrim.geometry = grim
         sceneView.scene.rootNode.addChildNode(nodeGrim)
         sceneView.autoenablesDefaultLighting = true
     }
     
     func setupFire1(){
         
-        let planeGeometry = SCNPlane(width: 15, height: 15)
+        let planeGeometry = SCNPlane(width: 1, height: 1)
         let material = SCNMaterial()
         material.diffuse.contents = UIImage(named: "FireballB_Dead.png")
         planeGeometry.materials = [material]
 
-        let FireKiri = SCNNode(geometry: planeGeometry)
-        FireKiri.position = SCNVector3(x : -1, y: 1.5, z : -5)
+        
+        FireKiri.geometry = planeGeometry
+        FireKiri.name = "fireKiri"
+        FireKiri.position = SCNVector3(x : -1, y: 2, z : -4)
         sceneView.scene.rootNode.addChildNode(FireKiri)
         sceneView.autoenablesDefaultLighting = true
     }
@@ -458,11 +444,12 @@ class GamePlayViewController: UIViewController, ARSCNViewDelegate {
     func setupFire2(){
         let planeGeometry = SCNPlane(width: 5, height: 5)
         let material = SCNMaterial()
-        material.diffuse.contents = UIImage.gif(name: "FireballARemake")
+        material.diffuse.contents = UIImage(named: "FireballA")
         planeGeometry.materials = [material]
         
         let FireKanan = SCNNode(geometry: planeGeometry)
-        FireKanan.position = SCNVector3(x : 1.5, y: 3, z : -5)
+        FireKanan.name = "fireKanan"
+        FireKanan.position = SCNVector3(x : 1.5, y: 3, z : -4)
         sceneView.scene.rootNode.addChildNode(FireKanan)
         sceneView.autoenablesDefaultLighting = true
     }
@@ -577,6 +564,9 @@ extension GamePlayViewController: UICollectionViewDelegate, UICollectionViewData
                 == 0 && inventoryItem[0] != "" {
                     hammerIsSelected = true
                 }
+                else if indexPath.row == 1 && inventoryItem[1] != "" {
+                    fragmentIsSelected = true
+                }
                 isSelected = false
             } else {
                 selectedCell.contentView.backgroundColor = .clear
@@ -584,6 +574,10 @@ extension GamePlayViewController: UICollectionViewDelegate, UICollectionViewData
                 if indexPath.row
                 == 0 && inventoryItem[0] != "" {
                     hammerIsSelected = false
+                }
+                else if indexPath.row
+                == 1 && inventoryItem[1] != "" {
+                    fragmentIsSelected = false
                 }
             }
         }
